d39d875967504823d8980e159f2c56e56f106908
Dead2309 <nathan.fauchere23@gmail.com>
2025-04-15 18:05:24 +0200

    Ajout d'un script build pour Render

---
+++ index.mjs
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApS-K-35D7_XNQf1aVuKL5gQF_fvfzwI0",
+  authDomain: "portfolio-a7e91.firebaseapp.com",
+  projectId: "portfolio-a7e91",
+  storageBucket: "portfolio-a7e91.firebasestorage.app",
+  messagingSenderId: "19885692738",
+  appId: "1:19885692738:web:f2f9f7a6f2957fdc945adb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);  
